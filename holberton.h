72c36ab21e233634581e3d2c5b863deb9aa507a4
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-04-14 20:07:54 +0000

    prototypes

---
+++ holberton.h
@@ -0,0 +1,55 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/**
+ * struct Pf_func - format
+ * _printf- print function
+ * @op: pointer
+ * @f: function pointer
+*/
+typedef struct Pf_func
+{
+	char *op;
+	int (*f)(va_list);
+} print_fun;
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
+int _putchar(char c);
+char **_split(char *av);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _strcmp(const char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int (*get_op_func(const char *s, int pos))(va_list);
+char *list_path(char array[], char *environ[]);
+char *_strdup(char *str);
+char *_getenv(const char *name);
+
+#endif /* HOLBERTON_H */
