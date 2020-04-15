b548e9bb30647550ee6850cc7dc4d77aa97a847f
VMP1312 <victor.paz1312@hotmail.com>
2020-04-15 02:26:07 +0000

    Fix

--- _environ.c
+++ _environ.c
@@ -1,8 +1,8 @@
-include "holberton.h"
+#include "holberton.h"
 
 /**
  * _environ - prints the environment
- *@environ: environ 
+ *@environ: env 
  * Return: Always 0.
  */
 int _environ(char *environ[])
@@ -12,8 +12,10 @@ int _environ(char *environ[])
     i = 0;
     while (environ[i] != NULL)
     {
-        _printf("%s\n", environ[i]);
+        write (STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write (STDOUT_FILENO,"\n", 1);
         i++;
     }
+	
     return (0);
 }
