38729a2ad3181092c81cfadd42d93f8fc8e76a7c
VMP1312 <victor.paz1312@hotmail.com>
2020-04-15 17:59:51 +0000

    fixed exit status

--- shell.c
+++ shell.c
@@ -72,7 +72,7 @@ int main(int ac, char **av, char *environ[])
 	pid_t child;
 	char **array = NULL, *buff = NULL, errors[50];
 	size_t len = 0;
-	int status = 0, execs = 0;
+	int status = 0, execs = 0, exvalue = 0;
 
 	(void) ac;
 	
@@ -82,7 +82,7 @@ int main(int ac, char **av, char *environ[])
 		if (getline(&buff, &len, stdin) != EOF)
 		{
 			if (!_strcmp(buff, "exit\n"))
-				free(buff), exit(EXIT_SUCCESS);
+				free(buff), exit(exvalue);
 
 			else if (!_strcmp(buff, "env\n"))
 			_environ(environ);
@@ -99,14 +99,16 @@ int main(int ac, char **av, char *environ[])
 					{	sprintf(errors, "%s: %d: %s: not found\n", av[0], execs, array[0]);
 						write(STDERR_FILENO, errors, _strlen(errors)), free(buff);
 						doublefree(array);
-						exit(128);
+						exit(127);
 					}
 				} else
-					wait(&status), doublefree(array);	}
+					wait(&status), doublefree(array);	
+					if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+					exvalue = WEXITSTATUS(status);	}
 		} else
 		{
 			isatty(STDIN_FILENO) ? write(STDOUT_FILENO, "\n", 1) : status;
-			free(buff), exit(EXIT_SUCCESS);
+			free(buff), exit(exvalue);
 		}
-	} return (0);
+	} return (exvalue);
 }
