cfd7890b10d2e1edb2cfd074761ff7e42e37d337
VMP1312 <victor.paz1312@hotmail.com>
2020-04-15 02:01:02 +0000

    Fix

--- _strcmp.c
+++ _strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(const char *s1, char *s2)
 	for (i = 0 ; s1[i] != '\0' i++)
 	{	
 		if(s1[i] && !s2[i])
-			return (0);
+			return (1);
 		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
