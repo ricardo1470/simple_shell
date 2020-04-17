ed9b73b7799b3d45dfb11f7207693e0c486d70cb
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-04-17 04:13:59 +0000

    simple_shell

--- _exec.c
+++ _exec.c
@@ -11,26 +11,67 @@
  */
 int x(char **av, char **environ, char **array, char *buff, int *pt, int status)
 {
-pid_t child;
-char errors[50];
-int i = 0, execs = 0;
+	pid_t child;
+	char errors[50];
+	int i = 0, execs = 0;
+	struct stat st;
 
-child = fork(), execs++;
-if (child == 0)
-{
+	(void)buff;
 	array[0] = list_path(array[0], environ);
-	if (execve(array[0], array, environ) == -1)
-	{	sprintf(errors, "%s: %d: %s: not found\n", av[0], execs, array[0]);
-		write(STDERR_FILENO, errors, _strlen(errors)), free(buff);
+	if((stat(array[0], &st) == 0) && (access(array[0], X_OK) == 0))
+	{
+		child = fork();
+		execs++;
+	}
+	else if (access(array[0], X_OK) == 0)
+	{
+		sprintf(errors, "%s: %d: %s: Permission denied\n", av[0], execs, array[0]);
+		write(STDERR_FILENO, errors, _strlen(errors));
 		doublefree(array);
 		i = 0;
 		while (errors[i])
+		{
 			errors[i++] = 0;
-		exit(127);
+		}
+		*pt = 126;
+		return (0);
 	}
-} else
-	wait(&status), doublefree(array);
-	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-	*pt = WEXITSTATUS(status);
+	else
+	{
+		sprintf(errors, "%s: %d: %s: not found\n", av[0], execs, array[0]);
+		write(STDERR_FILENO, errors, _strlen(errors));
+		doublefree(array);
+		i = 0;
+		while (errors[i])
+			{
+				errors[i++] = 0;
+			}
+		*pt = 127;
+		return (0);
+	}
+	if (child == 0)
+	{
+		if (execve(array[0], array, environ) == -1)
+		{
+			sprintf(errors, "%s: %d: %s: not found\n", av[0], execs, array[0]);
+			write(STDERR_FILENO, errors, _strlen(errors));
+			doublefree(array);
+			i = 0;
+			while (errors[i])
+			{
+				errors[i++] = 0;
+			}
+			exit(127);
+		}
+	}
+	else
+	{
+		wait(&status), doublefree(array);
+		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+		{
+			*pt = WEXITSTATUS(status);
+		}
+		return (0);
+	}
 	return (0);
 }
